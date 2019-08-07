a5649d974853f8ff8506a78b015e20aab0e09cb3
harryub <harryub@ub.com>
2019-08-07 16:57:09 +0800

    add more test cases

--- setup.py
+++ setup.py
@@ -1,11 +1,21 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
         name='hdao-python-sdk',
         version='1.0.1+a1',
         description='Python3 implemented SDK for HyperDao',
+        long_description=long_description,
+        long_description_content_type="text/markdown",
         author='realm520',
         author_email='realm520@gmail.com',
         url='https://github.com/hyperdao/stable_coin_python_sdk',
-        packages=['hdao'],
+        packages=setuptools.find_packages(),
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+        ],
 )
--- tests/test_price_feeder.py
+++ tests/test_price_feeder.py
@@ -8,3 +8,27 @@ def test_get_owner():
     pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', api)
     owner = pf.get_owner()
     assert(owner['result'] == 'HXNWj42PcH3Q2gEQ9GnVV2y87qsXd8MCL85W')
+
+def test_get_state():
+    api = HXWalletApi(name='test_get_state', rpc_url='http://192.168.1.121:8077/')
+    pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', api)
+    owner = pf.get_state()
+    assert(owner['result'] == 'COMMON')
+
+def test_get_price():
+    api = HXWalletApi(name='test_get_price', rpc_url='http://192.168.1.121:8077/')
+    pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', api)
+    owner = pf.get_price()
+    assert(owner['result'] == '2.5')
+
+def test_get_base_asset():
+    api = HXWalletApi(name='test_get_base_asset', rpc_url='http://192.168.1.121:8077/')
+    pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', api)
+    owner = pf.get_base_asset()
+    assert(owner['result'] == 'BTC')
+
+def test_get_quote_asset():
+    api = HXWalletApi(name='test_get_quote_asset', rpc_url='http://192.168.1.121:8077/')
+    pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', api)
+    owner = pf.get_quote_asset()
+    assert(owner['result'] == 'HXCcuGJV3cVnwMPk4S524ADcC9PWxRA3qKR2')
