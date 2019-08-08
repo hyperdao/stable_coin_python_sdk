2b7d1a1b86fea8bce2c93e0cb9aa364613886d03
harryub <harryub@ub.com>
2019-08-08 15:37:08 +0800

    add more test cases

--- tests/test_price_feeder.py
+++ tests/test_price_feeder.py
@@ -3,32 +3,37 @@ from hdao.hx_wallet_api import HXWalletApi
 from hdao.hdao_price_feeder import PriceFeeder
 
 
-def test_get_owner():
-    api = HXWalletApi(name='test_get_owner', rpc_url='http://192.168.1.121:8077/')
-    pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', api)
-    owner = pf.get_owner()
-    assert(owner['result'] == 'HXNWj42PcH3Q2gEQ9GnVV2y87qsXd8MCL85W')
+api = None
 
-def test_get_state():
-    api = HXWalletApi(name='test_get_state', rpc_url='http://192.168.1.121:8077/')
-    pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', api)
-    owner = pf.get_state()
-    assert(owner['result'] == 'COMMON')
+class TestPriceFeeder():
+    def setup_method(self, function):
+        self.api = HXWalletApi(name=function.__name__, rpc_url='http://192.168.1.121:8077/')
 
-def test_get_price():
-    api = HXWalletApi(name='test_get_price', rpc_url='http://192.168.1.121:8077/')
-    pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', api)
-    owner = pf.get_price()
-    assert(owner['result'] == '2.5')
+    def teardown_function(self):
+        self.api = None
 
-def test_get_base_asset():
-    api = HXWalletApi(name='test_get_base_asset', rpc_url='http://192.168.1.121:8077/')
-    pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', api)
-    owner = pf.get_base_asset()
-    assert(owner['result'] == 'BTC')
 
-def test_get_quote_asset():
-    api = HXWalletApi(name='test_get_quote_asset', rpc_url='http://192.168.1.121:8077/')
-    pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', api)
-    owner = pf.get_quote_asset()
-    assert(owner['result'] == 'HXCcuGJV3cVnwMPk4S524ADcC9PWxRA3qKR2')
+    def test_get_owner(self):
+        pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', self.api)
+        owner = pf.get_owner()
+        assert(owner['result'] == 'HXNWj42PcH3Q2gEQ9GnVV2y87qsXd8MCL85W')
+
+    def test_get_state(self):
+        pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', self.api)
+        owner = pf.get_state()
+        assert(owner['result'] == 'COMMON')
+
+    def test_get_price(self):
+        pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', self.api)
+        owner = pf.get_price()
+        assert(owner['result'] == '2.5')
+
+    def test_get_base_asset(self):
+        pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', self.api)
+        owner = pf.get_base_asset()
+        assert(owner['result'] == 'BTC')
+
+    def test_get_quote_asset(self):
+        pf = PriceFeeder('da', 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1', self.api)
+        owner = pf.get_quote_asset()
+        assert(owner['result'] == 'HXCcuGJV3cVnwMPk4S524ADcC9PWxRA3qKR2')
