d874f54a05a10841d90df321e213fdb1a6aba430
harryub <harryub@ub.com>
2019-08-23 18:27:27 +0800

    upload to pipy and update document.

--- README.md
+++ README.md
@@ -2,7 +2,28 @@
 Python SDK for HyperDao Stable Coin System
 
 
-# test
+# Install
+
+1. Using ```pip```
+
+```
+pip install hdao-python-sdk
+```
+
+2. Install from source
+
+```
+git clone https://github.com/hyperdao/stable_coin_python_sdk
+cd stable_coin_python_sdk
+python setup.py install
+```
+
+# Usage
+
+# Test
+
+HyperDao stable coin system depends on HyperExchange mainnet. So the test cases should be run with HyperExchange testnet setup.
+Please update the configuraions about HyperExchange testnet.
 
 ```
 cd tests
--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
         name='hdao-python-sdk',
-        version='1.0.1+a1',
+        version='1.0.1b1',
         description='Python3 implemented SDK for HyperDao',
         long_description=long_description,
         long_description_content_type="text/markdown",
         author='realm520',
         author_email='realm520@gmail.com',
         url='https://github.com/hyperdao/stable_coin_python_sdk',
-        packages=setuptools.find_packages(),
+        packages=find_packages(),
         classifiers=[
             "Programming Language :: Python :: 3",
             "License :: OSI Approved :: MIT License",
