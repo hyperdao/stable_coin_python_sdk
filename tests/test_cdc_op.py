d081154e34441ccb5a8a0c3e1f08e81a685460e6
harryub <harryub@ub.com>
2019-10-17 17:01:54 +0800

    update test case

--- tests/test_cdc_op.py
+++ tests/test_cdc_op.py
@@ -1,4 +1,4 @@
-import pytest
+import pytest, json
 from hdao.hx_wallet_api import HXWalletApi
 from hdao.hdao_cdc_op import CDCOperation
 from .config import USER1, CDC_CONTRACT_ID, HX_TESTNET_RPC
@@ -15,5 +15,19 @@ class TestCdcOp():
 
     def test_info(self):
         info = self.cdc.get_contract_info()
-        assert(info == "{\"admin\":\"HXNWj42PcH3Q2gEQ9GnVV2y87qsXd8MCL85W\",\"proxy\":\"HXCexPawducWo8S5uPTtMbzoN31tpxGxwV3H\",\"state\":\"COMMON\",\"collateralAsset\":\"BTC\",\"priceFeederAddr\":\"HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1\",\"stableTokenAddr\":\"HXCcuGJV3cVnwMPk4S524ADcC9PWxRA3qKR2\",\"liquidationRatio\":\"1.25\",\"annualStabilityFee\":\"0.15\",\"liquidationPenalty\":\"0.13\",\"liquidationDiscount\":\"0.03\",\"annualStabilityFeeList\":[\"1564047710,0.15\"],\"totalLiquidationPenalty\":0,\"totalCollectedStablityFee\":0}")
+        info = json.loads(info)
+        assert(info['admin'] == 'HXNWj42PcH3Q2gEQ9GnVV2y87qsXd8MCL85W')
+        assert(info['proxy'] == 'HXCexPawducWo8S5uPTtMbzoN31tpxGxwV3H')
+        assert(info['state'] == 'COMMON')
+        assert(info['collateralAsset'] == 'BTC')
+        assert(info['priceFeederAddr'] == 'HXCGba6bUaGeBtUQRGpHUePHVXzF1ygMAxR1')
+        assert(info['stableTokenAddr'] == 'HXCcuGJV3cVnwMPk4S524ADcC9PWxRA3qKR2')
+        assert(info['liquidationRatio'] == '1.25')
+        assert(info['annualStabilityFee'] == '0.15')
+        assert(info['liquidationPenalty'] == '0.13')
+        assert(info['liquidationDiscount'] == '0.03')
+        assert(info['annualStabilityFeeList'] == ["1564047710,0.15"])
+        assert(int(info['totalLiquidationPenalty']) >= 0)
+        assert(int(info['totalCollectedStablityFee']) >= 0)
+
 
