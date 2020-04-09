e0cb4695b069e05e9280e0b9720873ae818b7e9b
mainByte01 <62131071+mainByte01@users.noreply.github.com>
2020-04-09 19:21:54 +0800

    Add files via upload
    add main file

---
+++ mainnet/main.py
@@ -0,0 +1,11 @@
+from  mainnet.collect_event import *
+from mainnet.config import *
+from mainnet.feeder import *
+
+
+if __name__ == '__main__':
+    '''need to launch all program 
+    '''
+    while True :
+
+        collector = HdaoEventsCollector()
