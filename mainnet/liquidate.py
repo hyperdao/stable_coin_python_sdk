b45124ee697088d6e28ea3ddbfa9c7876775d501
DanNathan001 <62128723+DanNathan001@users.noreply.github.com>
2020-05-03 23:05:41 +0800

    Update liquidate.py
    add logger for liquidatorFactory

--- mainnet/liquidate.py
+++ mainnet/liquidate.py
@@ -90,7 +90,7 @@ class Cdc_Liquidate():
 class HDaoLiquidateFactor(threading.Thread) :
     '''need to be changed to a '''
     def loadConfigFile(self):
-        with open(self.robot_config_filepath, 'r') as f:
+        with open(self.config_filepath, 'r') as f:
             try:
                 self.jsonconfigs = json.load(f)
             except BaseException as e:
@@ -104,8 +104,18 @@ class HDaoLiquidateFactor(threading.Thread) :
         self.session = session
         self.api = api
         self.robots = []
-        self.config_path = robot_config_filepath
+        self.config_filepath = robot_config_filepath
         self.jsonconfigs = {}
+        self.logger = logging.getLogger("priceFeedingRobot")
+        self.logger.setLevel(level=logging.INFO)
+        handler = logging.FileHandler("priceFeedingRobot_log.txt")
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+        self.logger.info("Start print log")
+
+
     def stop(self):
         if len(self.robots) <=0 :
             print("there is no need to stop")
