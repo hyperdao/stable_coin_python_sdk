1914c5d03eb25eb0402aaefafd001db671b1eccb
DanNathan001 <62128723+DanNathan001@users.noreply.github.com>
2020-05-12 19:38:36 +0800

    Update liquidate.py
    update configrations about liquidate info

--- mainnet/liquidate.py
+++ mainnet/liquidate.py
@@ -90,7 +90,7 @@ class Cdc_Liquidate():
 class HDaoLiquidateFactor(threading.Thread) :
     '''need to be changed to a '''
     def loadConfigFile(self):
-        with open(self.config_filepath, 'r') as f:
+        with open(self.robot_config_filepath, 'r') as f:
             try:
                 self.jsonconfigs = json.load(f)
             except BaseException as e:
@@ -104,18 +104,8 @@ class HDaoLiquidateFactor(threading.Thread) :
         self.session = session
         self.api = api
         self.robots = []
-        self.config_filepath = robot_config_filepath
+        self.config_path = robot_config_filepath
         self.jsonconfigs = {}
-        self.logger = logging.getLogger("priceFeedingRobot")
-        self.logger.setLevel(level=logging.INFO)
-        handler = logging.FileHandler("priceFeedingRobot_log.txt")
-        handler.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
-        self.logger.info("Start print log")
-
-
     def stop(self):
         if len(self.robots) <=0 :
             print("there is no need to stop")
@@ -127,11 +117,11 @@ class HDaoLiquidateFactor(threading.Thread) :
             print("error, robots has been started")
         try:
             self.loadConfigFile()
-            liquidate_info = self.jsonconfigs["Liquidate_info"]
+            liquidate_info = self.jsonconfigs["cdc_contract_info"]
             global_info = self.jsonconfigs["global_info"]
-            for i in  range(0,len(liquidate_info)) :
-                robot = Cdc_Liquidate(self.api,global_info["SQLDB"],global_info["CDC_ID"],global_info["ACCOUNT"],
-                                       global_info["STABLEPRECISION"],global_info["COLLECTEALPRECISION"],self.session)
+            for k, v in liquidate_info.items():
+                robot = Cdc_Liquidate(self.api,global_info["SQLDB"],v["CDC_CONTRACT_ID"],global_info["ACCOUNT"],
+                                       global_info["STABLEPRECISION"],v["COLLECTEALPRECISION"],self.session)
                 self.robots.append(robot)
         except:
             print("xxxx")
